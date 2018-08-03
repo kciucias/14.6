100664dab38eba746f57d5f2348360a3a0a6fdda
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-08-03 19:04:45 +0100

    Add counter

---
+++ script.js
@@ -0,0 +1,24 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {onClick: this.increment},
+            React.createElement('button', {}, 'Licznik ' + this.state.counter)
+        );
+    }
+});
+
+
+var app = React.createElement(Counter);
+
+ReactDOM.render(app, document.getElementById('app'));
